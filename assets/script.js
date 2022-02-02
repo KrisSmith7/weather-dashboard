d4e791bbddacdf28cca65dd4cb3aa052cdb3afb1
KrisSmith7 <krissmithcodes@gmail.com>
2022-02-02 13:35:59 -0500

    debugging display dates for forecast cards

--- assets/script.js
+++ assets/script.js
@@ -6,12 +6,22 @@ var weatherContainer = document.querySelector("#weather-container")
 var weatherList = document.querySelector("#weather-list")
 
 var currentName = document.querySelector('#current-city-name');
+var currentDate = document.querySelector('#current-date');
 var currentWind = document.querySelector('#current-city-wind');
 var currentHumidity = document.querySelector('#current-city-humidity');
 var currentTemp = document.querySelector('#current-city-temp');
 var currentUV = document.querySelector('#current-city-uv');
+var uvContainer = document.querySelector('#current-index');
 var currentIcon = document.querySelector('#current-wIcon');
 const APIkey = 'b95de3ec77908dd1b6d987025a729e40'
+
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const d = new Date();
+let today = weekday[d.getDay()];
+
+
+var prevSearchContainer = document.querySelector('#prev-searches')
+var searchedCities = JSON.parse(localStorage.getItem("SearchedCities")) || []; // key: SearchedCities: "["{"austin"}"]"
 
 
 var formSubmitHandler = function (event) {
@@ -26,11 +36,32 @@ var formSubmitHandler = function (event) {
         currentName.textContent = cityname;
 
         cityInputEl.value = '';
+        searchedCities.push(cityname);
+        saveCitySearch();
+        displaySearchHistory()
     } else {
         alert('Please enter a city to search.');
     }
 };
 
+var saveCitySearch = function() {
+    localStorage.setItem("SearchedCities", JSON.stringify(searchedCities));
+};
+
+var displaySearchHistory = function() {
+    let currentCities = JSON.parse(localStorage.getItem("SearchedCities"));
+    // console.log (currentCities);
+      
+
+    //create button for each city searched
+    for (var i = 0; i < currentCities.length; i++) {
+        var newButton = document.createElement("button");
+        newButton.textContent = currentCities[i];
+        newButton.classList = "btn btn-secondary btn-lg my-2 p-2 w-100";
+        // cityEl.setAttribute("data-city", cities[i]);
+        prevSearchContainer.appendChild(newButton);
+    }
+ };
 
 var getCityWeather = function (city) {
     var apiURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey
@@ -61,25 +92,59 @@ var getCityWeather = function (city) {
   */
 var displayWeather = function(data) {
 
-    //how can I display this data on screen
+    //displays weather conditions on screen
     var currentIconNumber = data.current.weather[0].icon;
     currentIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + currentIconNumber + "@2x.png");
    
+    currentDate.textContent = today;
     currentTemp.textContent = "Temp: " + data.current.temp + " F";
     currentHumidity.textContent =  "Humidity: " + data.current.humidity + "%";
     currentWind.textContent = "Wind Speed: " + data.current.wind_speed + " MPH";
     currentUV.textContent = "UV Index: " + data.current.uvi;
 
+    
+    // conditional logic for that presents a color that indicates whether the UV index conditions are favorable, moderate, or severe
+    if (data.current.uvi <= 2.99) {
+        uvContainer.setAttribute("class", "rounded-md bg-green-900 text-green-50 font-bold p-2");
+        uvContainer.innerText = "Favorable"
+    }
+    if (data.current.uvi >= 3 && data.current.uvi <= 6){
+        uvContainer.setAttribute("class", "rounded-md bg-yellow-700 text-yellow-50 font-bold p-2");
+        uvContainer.innerText = "Moderate"
+    }
+    if (data.current.uvi >=6.01){
+        uvContainer.setAttribute("class", "rounded-md bg-red-900 text-red-50 font-bold p-2");
+        uvContainer.innerText = "Severe"
+    }
+
+    fiveDayContainerEl.innerHTML = ""
 
     for (var i = 0; i < 5; i++){
-       
+        
+        let curr = new Date 
+    let week = []
+    
+    for (let i = 1; i <= 7; i++) {
+      let first = curr.getDate() - curr.getDay() + i 
+      let day = new Date(curr.setDate(first)).toISOString().slice(0, 10)
+      week.push(day)
+    }
+    console.log(week);
+   
+        
+    // let day = weekday[d.getDay()];
+    // console.log(d)
+
       var cardContainer = document.createElement("div");
+      cardContainer.setAttribute("class", "px-8")
       var cardBody = document.createElement("div");
-      cardBody.setAttribute("class", "bg-dark p-3 border");
+      cardBody.setAttribute("class", "bg-gray-500 text-white border-2 rounded-md p-3");
   
       var cityDate = document.createElement("div");
-      // cityDate.textContent = moment().add((i+ 1), 'day').format("MMM DD YYYY");
-     
+      cityDate.textContent =  week[i]
+    //   const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    //   const d = new Date();
+    
   
       var weatherImage = document.createElement("img");
       var iconNumber = data.daily[i].weather[0].icon;
@@ -99,7 +164,7 @@ var displayWeather = function(data) {
     
       cardBody.append(cityDate, weatherImage, tempElement, windSpeed, humidityF);
       cardContainer.append(cardBody);
-      document.querySelector("#forecast-container").append(cardContainer);
+      fiveDayContainerEl.append(cardContainer);
     }
   }
 
@@ -113,10 +178,7 @@ THEN I am presented with current and future conditions for that city and that ci
 */
 
 
-/*
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-*/
+
 
 /*
 WHEN I view future weather conditions for that city
